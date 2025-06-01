aa22ff2ae84468cb0d53b851d32ae7042ec653d6
Mercyh <abdullamercyh@hotmail.com>
2025-06-02 04:29:11 +0500

    Add css & configure components

---
+++ src/components/links/Links.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './links.css';
+
+
+const Links = () => {
+  return(
+  <div>
+    <h1>Links</h1>
+  </div>
+  )
+}
+
+export default Links
---
+++ src/components/technologies/Technologies.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './technologies.css';
+
+
+const Technologies = () => {
+  return(
+  <div>
+    <h1>Technologies</h1>
+  </div>
+  )
+}
+
+export default Technologies 
